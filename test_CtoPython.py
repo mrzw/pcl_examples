2a1b78071e5ca1be3961bafce8c08220fa4d1e41
mrzw <2463846947@qq.com>
2018-01-27 13:01:02 +0800

    Update test_CtoPython.py

--- test_CtoPython.py
+++ test_CtoPython.py
@@ -15,7 +15,7 @@ def geticp(rgbfile, depthfile, pose):
     lib = cdll.LoadLibrary('××.so')
     func = lib.icp                          # icp是C++程序对应的函数
     func.restype = c_double                 # 设置返回值类型，默认输出int型
-    func.restype = POINTER(c_double * 16)   # 当返回是个array(这里以输出16维度为例)
+    func.restype = POINTER(c_double * 16)   # 当返回是个array(这里以输出16维度为例), 获取返回值: ans[0][0] ... ans[0][15]
     pose = (c_double * len(pose))(*pose)    # 当输入是个list表的浮点数
     rgbfile = rgbfile.encode('utf-8')       # 传入的字符串参数必须为encode('utf-8')，否则在c库中仅会打印为首字符
     depthfile = depthfile.encode('utf-8')
